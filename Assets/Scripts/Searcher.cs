7bb47d16358fa0f97daedeba825313970f9bb639
terrygonguet <terry@gonguet.com>
2019-04-13 02:26:10 +0200

    Base FPS controls

--- Assets/Scripts/Searcher.cs
+++ Assets/Scripts/Searcher.cs
@@ -2,21 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Hider : MonoBehaviour
+public class Searcher : MonoBehaviour
 {
     public float Speed = 500;
     public float JumpForce = 400;
-    public float CameraDistance = 7;
 
     private Rigidbody body;
     private new Camera camera;
-    private Vector2 cameraRotation = new Vector2(20, 0);
-        
-    void Start() {
+    private Vector2 cameraRotation = new Vector2(0, 0);
+
+    void Start()
+    {
         body = GetComponent<Rigidbody>();
         camera = GetComponentInChildren<Camera>();
     }
-    
+
     void Update()
     {
         Vector3 move = transform.forward * Input.GetAxis("Vertical") + transform.right * Input.GetAxis("Horizontal");
@@ -30,15 +30,12 @@ public class Hider : MonoBehaviour
             body.AddForce(0, JumpForce, 0);
         }
 
-        CameraDistance = Mathf.Clamp(Input.GetAxis("Mouse ScrollWheel") + CameraDistance, 1, 10);
-
         cameraRotation = new Vector2(
-            Mathf.Clamp(Input.GetAxis("Mouse Y") + cameraRotation.x, -5, 40),
+            Mathf.Clamp(Input.GetAxis("Mouse Y") + cameraRotation.x, -85, 85),
             (Input.GetAxis("Mouse X") + cameraRotation.y) % 360
         );
 
-        camera.transform.localPosition = Quaternion.Euler(cameraRotation.x, 0, 0) * new Vector3(0, 1, -CameraDistance);
         transform.rotation = Quaternion.Euler(0, cameraRotation.y, 0);
-        camera.transform.LookAt(transform, Vector3.up);
+        camera.transform.localRotation = Quaternion.Euler(cameraRotation.x, 0, 0);
     }
 }
