32caeaa63b032ae0edd4c812b7169ab192ccfbce
Daydreel <daydreel@gmail.com>
2019-04-19 15:06:10 +0200

    Added Souris basic animation

--- Assets/Scripts/Souris.cs
+++ Assets/Scripts/Souris.cs
@@ -12,11 +12,14 @@ public class Souris : MonoBehaviour
     private new Camera camera;
     private bool isInAir;
 
+    private Animator anim;
+
     private Vector2 cameraRotation = new Vector2(20, 0);
         
     void Start() {
         body = GetComponent<Rigidbody>();
         camera = GetComponentInChildren<Camera>();
+        anim = GetComponent<Animator>();
     }
 
     void FixedUpdate()
@@ -31,10 +34,21 @@ public class Souris : MonoBehaviour
         }
 
         Vector3 movement = transform.forward * Input.GetAxis("Vertical") + transform.right * Input.GetAxis("Horizontal");
+        //Set animation
+        if (movement.x == 0 || movement.z == 0)
+        {
+            anim.SetBool("run", false);
+        }
+        else
+        {
+            anim.SetBool("run", true);
+        }
+
+
         movement *= Speed;
         movement *= Time.fixedDeltaTime;
 
-        transform.position += movement;
+        body.MovePosition(body.position + movement);
 
         cameraRotation = new Vector2(
             Mathf.Clamp(Input.GetAxis("Mouse Y") + cameraRotation.x, -5, 80),
